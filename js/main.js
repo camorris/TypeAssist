5e9a05b510538766808c8e22b78e9d431e9f3db9
Chase Morris <camorris@callutheran.edu>
2017-11-23 13:39:27 -0800

    got rid of jquery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var timeLeftElement = $('#timeLeft')
-var resetBtn = $('#clear')
-var pointsElementA = $('#pointsA')
-var pointsElementB = $('#pointsB')
-var playerTurn = $('.turn') 
+var timeLeftElement = document.getElementById('timeLeft')
+var resetBtn = document.getElementById('clear')
+var pointsElementA = document.getElementById('pointsA')
+var pointsElementB = document.getElementById('pointsB')
+var playerTurn = document.getElementsByClassName('turn') 
 var speed = 1000
 var score = 0 
 var seconds = 30
@@ -10,33 +10,45 @@ var playerAScore = null
 var playerBScore = null
 var timesPlayed = 0
 var currentPlayer = 'playerA' 
-var newBanana= $('#start')
+var newBanana= document.getElementById('start')
+var key = document.getElementsByClassName('key')
+// // var position = key.getBoundingClientRect();
+// var x = position.left;
+// var y = position.top;
+
+console.log(pointsElementA)
+
+
 
   //<---This click function allows the players to click on the Banana Image, after the Banana button is clicked--->//
-      window.addEventListener('keydown', function(){
+      function addScore(e){
+      
             if (currentPlayer === 'playerA'){
-          score = score + 10
-          pointsElementA.html("Score A: " + score )
+          score = score + 10;
+          pointsElementA.innerHTML = ("Score A: " + score )
       } else if (currentPlayer === 'playerB'){
           score = score + 10
-          pointsElementB.html("Score B: " + score)
+          pointsElementB.innerHTML = ("Score B: " + score)
           }
           // theIntervalId = setInterval(countDown, speed)
-          
-      })
-      theIntervalId = setInterval(countDown, speed)
+          if (e.propertyName !== 'transform')return;
+          if(!audio)return;
+
+          theIntervalId = setInterval(countDown, speed,)
+      }
+      
       
   //<---This if statement allows for score increases, size decreases, speed increases for each player--->//
-    function countDown(){
+    function countDown(e){
       seconds = seconds - 1;
-      timeLeftElement.html( "Time Left: " + seconds + " seconds");
+      timeLeftElement.innerHTML = ( "Time Left: " + seconds + " seconds");
       
   //<---This if statement makes sure once the timer hits 0 seconds the Player A turn is over and Player B can play--->
       if (seconds <= 0){
           currentPlayer = "playerB" //kyle
   // //<---This if statement allows us to see that PlayerB should be playing plus it puts the speed, interval and size back to its orginal properties--->//
           if (currentPlayer === "playerB"){
-          playerTurn.html('Player B')};
+          playerTurn.innerHTML = ('Player B')};
           clearInterval( theIntervalId );
           alert("Your score is " + score);
   // //<---This if statement makes sure that score based off how many times the banana was clicked is assigned to the right player the first time its played--->//
@@ -67,11 +79,12 @@ var newBanana= $('#start')
     }
 function playSound(e){
 const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 if(!audio)return;
 audio.currentTime = 0;
 audio.play();
 key.classList.add('playing');
+
 };
 
 function removeTransition(e){
@@ -81,4 +94,14 @@ this.classList.remove('playing');
 const keys = document.querySelectorAll('.key');
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
-window.addEventListener('keydown', playSound)
+
+  
+      
+     
+
+  console.log(key)
+
+
+window.addEventListener('keydown', playSound, )
+window.addEventListener('keydown', addScore,)
+window.addEventListener('keyup', removeTransition)
